4201e57abfd8b234987515657831b9ccb28347c1
gokul <gokulkrishnanml@gmail.com>
2021-07-04 18:41:26 +0530

    Django Rest Api Post get user details using token

--- restapi/restapi/urls.py
+++ restapi/restapi/urls.py
@@ -23,5 +23,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register',views.register.as_view(),name="register"),
-    path('login',obtain_auth_token,name="login")
+    path('login',obtain_auth_token,name="login"),
+    path('welcome',views.welcome.as_view(),name="welcome")
 ]
